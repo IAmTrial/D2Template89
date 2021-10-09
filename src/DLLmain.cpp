4613af545bde97ffe1efa1879ad4b5fa25515e97
Mir Drualga <IAmRealTrial+D2Template89@gmail.com>
2021-10-09 13:15:09 -0700

    Add D2Template89 license header

--- src/DLLmain.cpp
+++ src/DLLmain.cpp
@@ -1,7 +1,19 @@
-#define _D2VARS_H
-
-#include "DLLmain.h"
-#include "D2Patch.h"
+/**
+ * D2Template89
+ * Copyright 2021 Mir Drualga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /****************************************************************************
 *                                                                           *
@@ -26,6 +38,11 @@
 *   D2Template core file, do not modify unless you know what you're doing   *
 *                                                                           *
 *****************************************************************************/
+
+#define _D2VARS_H
+
+#include "DLLmain.h"
+#include "D2Patch.h"
 
 void __fastcall D2TEMPLATE_FatalError(char* szMessage)
 {
