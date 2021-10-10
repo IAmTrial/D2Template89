f8397eaf6a707f0734ec100cbae8514057e07b7a
Mir Drualga <IAmRealTrial+D2Template89@gmail.com>
2021-10-10 12:40:50 -0700

    Add DllError

---
+++ src/DllError.c
@@ -0,0 +1,71 @@
+/**
+ * D2Template89
+ * Copyright 2021 Mir Drualga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/****************************************************************************
+*                                                                           *
+*   DLLmain.h                                                               *
+*                                                                           *
+*   Licensed under the Apache License, Version 2.0 (the "License");         *
+*   you may not use this file except in compliance with the License.        *
+*   You may obtain a copy of the License at                                 *
+*                                                                           *
+*   http://www.apache.org/licenses/LICENSE-2.0                              *
+*                                                                           *
+*   Unless required by applicable law or agreed to in writing, software     *
+*   distributed under the License is distributed on an "AS IS" BASIS,       *
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+*   See the License for the specific language governing permissions and     *
+*   limitations under the License.                                          *
+*                                                                           *
+*****************************************************************************/
+
+#include "DllError.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <windows.h>
+
+#define MESSAGE_FORMAT L"File: %ls\n" \
+    L"Line: %d" \
+    L"%ls"
+
+/*
+ * This is allocated as a global, so calls to error functions do not
+ * trigger stack overflow error.
+ */
+static wchar_t full_message[D2TEMPLATE_Error_kMessageCapacity];
+
+/**
+ * External
+ */
+
+void D2TEMPLATE_ExitWithMessage(
+    const wchar_t* message,
+    const wchar_t* file,
+    int line) {
+
+  _snwprintf(
+      full_message,
+      D2TEMPLATE_Error_kMessageCapacity,
+      MESSAGE_FORMAT,
+      file,
+      line,
+      message);
+  MessageBoxW(NULL, L"D2Template Error", message, MB_OK | MB_ICONERROR);
+  exit(EXIT_FAILURE);
+}
---
+++ src/DllError.h
@@ -0,0 +1,62 @@
+/**
+ * D2Template89
+ * Copyright 2021 Mir Drualga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/****************************************************************************
+*                                                                           *
+*   DLLmain.h                                                               *
+*                                                                           *
+*   Licensed under the Apache License, Version 2.0 (the "License");         *
+*   you may not use this file except in compliance with the License.        *
+*   You may obtain a copy of the License at                                 *
+*                                                                           *
+*   http://www.apache.org/licenses/LICENSE-2.0                              *
+*                                                                           *
+*   Unless required by applicable law or agreed to in writing, software     *
+*   distributed under the License is distributed on an "AS IS" BASIS,       *
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+*   See the License for the specific language governing permissions and     *
+*   limitations under the License.                                          *
+*                                                                           *
+*****************************************************************************/
+
+#ifndef D2TEMPLATE89_DLLERROR_H_
+#define D2TEMPLATE89_DLLERROR_H_
+
+#include <wchar.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+enum {
+  /*
+   * An error message beyond 512 characters would be unreasonable. Try
+   * writing to stderr if more than 512 characters are needed.
+   */
+  D2TEMPLATE_Error_kMessageCapacity = 512,
+};
+
+void D2TEMPLATE_ExitWithMessage(
+    const wchar_t* message,
+    const wchar_t* file,
+    int line);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif /* D2TEMPLATE89_DLLERROR_H_ */
