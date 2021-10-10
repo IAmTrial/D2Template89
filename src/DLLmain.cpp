affeeed968615c8c91726b788480b9a73cbd2e87
Mir Drualga <IAmRealTrial+D2Template89@gmail.com>
2021-10-10 16:23:01 -0700

    Remove redundant and refactored code from DllMain

--- src/DLLmain.cpp
+++ src/DLLmain.cpp
@@ -39,168 +39,87 @@
 *                                                                           *
 *****************************************************************************/
 
-#define _D2VARS_H
+#include <windows.h>
 
-#include "DLLmain.h"
-#include "D2Patch.h"
-
-void __fastcall D2TEMPLATE_FatalError(char* szMessage)
-{
-    MessageBoxA(NULL, szMessage, "D2Template", MB_OK | MB_ICONERROR);
-    TerminateProcess(GetCurrentProcess(), -1);
-}
-
-BOOL __fastcall D2TEMPLATE_ApplyPatch(void* hGame, const DLLPatchStrc* hPatch)
-{
-    while (hPatch->nDLL != D2DLL_INVALID)
-    {
-        int nReturn = 0;
-        int nDLL = hPatch->nDLL;
-        if (nDLL < 0 || nDLL >= D2DLL_INVALID) return FALSE;
-        
-        DWORD dwAddress = hPatch->dwAddress;
-        if (!dwAddress) return FALSE;
-
-        DWORD dwBaseAddress = gptDllFiles[nDLL].dwAddress;
-        if (!dwBaseAddress) return FALSE;
-
-        dwAddress += dwBaseAddress;
-        
-        DWORD dwData = hPatch->dwData;
-        if (hPatch->bRelative){ dwData = dwData - (dwAddress + sizeof(dwData)); }
-        
-        void* hAddress = (void*)dwAddress;
-        DWORD dwOldPage;
-
-        if (hPatch->nPatchSize > 0)
-        {
-            BYTE Buffer[1024];
-
-            for (size_t i = 0; i < hPatch->nPatchSize; i++)
-                Buffer[i] = (BYTE)dwData;
-
-            VirtualProtect(hAddress, hPatch->nPatchSize, PAGE_EXECUTE_READWRITE, &dwOldPage);
-            nReturn = WriteProcessMemory(hGame, hAddress, &Buffer, hPatch->nPatchSize, 0);
-            VirtualProtect(hAddress, hPatch->nPatchSize, dwOldPage, 0);
-        }
-
-        else
-        {
-            VirtualProtect(hAddress, sizeof(dwData), PAGE_EXECUTE_READWRITE, &dwOldPage);
-            nReturn = WriteProcessMemory(hGame, hAddress, &dwData, sizeof(dwData), 0);
-            VirtualProtect(hAddress, sizeof(dwData), dwOldPage, 0);
-        }
-        
-        if (nReturn == 0) return FALSE;
-        
-        hPatch++;
-    }
-    
-    return TRUE;
-}
-
-BOOL __fastcall D2TEMPLATE_LoadModules()
-{
-    for (int i = 0; i < D2DLL_INVALID; i++)
-    {
-        DLLBaseStrc* hDllFile = &gptDllFiles[i];
-        
-        void* hModule = GetModuleHandle(hDllFile->szName);
-        if (!hModule)
-        {
-            hModule = LoadLibrary(hDllFile->szName);
-        }
-
-        hDllFile->dwAddress = (DWORD)hModule;
-    }
-
-    return TRUE;
-}
+#include "DllError.h"
+#include "patches.h"
 
 int __fastcall D2TEMPLATE_GetDebugPrivilege()
 {
-    void* hToken;
-    LUID luid;
-    TOKEN_PRIVILEGES tokenPrivileges;
+  void* hToken;
+  LUID luid;
+  TOKEN_PRIVILEGES tokenPrivileges;
 
-    if (OpenProcessToken(GetCurrentProcess(),TOKEN_ALL_ACCESS,&hToken) == 0)
-    {
-        D2TEMPLATE_FatalError("OpenProcessToken Failed");
-        return 0;
-    }
+  if (OpenProcessToken(GetCurrentProcess(),TOKEN_ALL_ACCESS,&hToken) == 0)
+  {
+    D2TEMPLATE_ExitWithMessage(
+        L"OpenProcessToken Failed",
+        __FILEW__,
+        __LINE__);
+    return 0;
+  }
 
-    if (LookupPrivilegeValue(0,SE_DEBUG_NAME,&luid) == 0)
-    {
-        D2TEMPLATE_FatalError("LookupPrivilegeValue Failed");
-        CloseHandle(hToken);
-        return 0;
-    }
-
-    tokenPrivileges.PrivilegeCount = 1;
-    tokenPrivileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-    tokenPrivileges.Privileges[0].Luid = luid;
-    if (AdjustTokenPrivileges(hToken,0,&tokenPrivileges,sizeof(tokenPrivileges),0,0) == 0)
-    {
-        D2TEMPLATE_FatalError("AdjustTokenPrivileges Failed");
-        CloseHandle(hToken);
-        return 0;
-    }
-
+  if (LookupPrivilegeValue(0,SE_DEBUG_NAME,&luid) == 0)
+  {
+    D2TEMPLATE_ExitWithMessage(
+        L"LookupPrivilegeValue Failed",
+        __FILEW__,
+        __LINE__);
     CloseHandle(hToken);
-    return 1;
+    return 0;
+  }
+
+  tokenPrivileges.PrivilegeCount = 1;
+  tokenPrivileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+  tokenPrivileges.Privileges[0].Luid = luid;
+  if (AdjustTokenPrivileges(hToken,0,&tokenPrivileges,sizeof(tokenPrivileges),0,0) == 0)
+  {
+    D2TEMPLATE_ExitWithMessage(
+        L"AdjustTokenPrivileges Failed",
+        __FILEW__,
+        __LINE__);
+    CloseHandle(hToken);
+    return 0;
+  }
+
+  CloseHandle(hToken);
+  return 1;
 }
 
 int __stdcall DllAttach()
 {
-    D2TEMPLATE_GetDebugPrivilege();
+  D2TEMPLATE_GetDebugPrivilege();
 
-    void* hGame = GetCurrentProcess();
-    if (!hGame) 
-    {
-        D2TEMPLATE_FatalError("Failed to retrieve process");
-        return 0;
-    }
+  void* hGame = GetCurrentProcess();
+  if (!hGame) 
+  {
+    D2TEMPLATE_ExitWithMessage(
+        L"Failed to retrieve process",
+        __FILEW__,
+        __LINE__);
+    return 0;
+  }
 
-    if (!D2TEMPLATE_LoadModules())
-    {
-        D2TEMPLATE_FatalError("Failed to load modules");
-        return 0;
-    }
+  Patches_Apply();
 
-    D2TEMPLATE_ApplyPatch(hGame, gptTemplatePatches);
-
-    return 1;
+  return 1;
 }
 
 int __stdcall DllMain(HINSTANCE hModule, DWORD dwReason, void* lpReserved)
 {
-    switch (dwReason)
+  switch (dwReason)
+  {
+    case DLL_PROCESS_ATTACH:
     {
-        case DLL_PROCESS_ATTACH:
-        {
-            if (!DllAttach()) D2TEMPLATE_FatalError("Couldn't attach to Diablo II");
-            break;
-        }
+      if (!DllAttach()) {
+        D2TEMPLATE_ExitWithMessage(
+            L"Couldn't attach to Diablo II",
+            __FILEW__,
+            __LINE__);
+      }
+      break;
     }
+  }
 
-    return TRUE;
-}
-
-DWORD __fastcall GetDllOffset(char* ModuleName, DWORD BaseAddress, int Offset)
-{
-	if(!BaseAddress)
-		BaseAddress = (DWORD)LoadLibraryA(GetModuleExt(ModuleName));
-
-	if(Offset < 0)
-		return (DWORD)GetProcAddress((HMODULE)BaseAddress,(LPCSTR)(-Offset));
-
-	return BaseAddress + Offset;
-}
-
-char* __fastcall GetModuleExt(char* ModuleName)
-{
-	char DLLExt[] = ".dll";
-	char DLLName[32] = {0};
-	strcpy(DLLName,ModuleName);
-	return strcat(DLLName,DLLExt);
+  return TRUE;
 }
